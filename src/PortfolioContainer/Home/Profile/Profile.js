8e18fe44d5f39cae773349f14c9f8d8378afcbc8
Sagor Hossain <sagorhassain4@gmail.com>
2022-06-08 16:29:51 +0600

    resume as education done

--- src/PortfolioContainer/Home/Profile/Profile.js
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -6,65 +6,67 @@ import { Button } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <div className="profile-container">
-      <div className="profile-parent">
-        <div className="profile-details">
-          <div className="colz">
-            <div className="colz-icon">
-              <a href="https://web.facebook.com/md.sagor.rzs/" target="_blank">
-                <i className="fa fa-facebook-square"></i>
-              </a>
-              <a href="https://github.com/moh-sagor" target="_blank">
-                <i className="fa fa-github-square"></i>
-              </a>
-              <a href="https://twitter.com/s0707Sagor" target="_blank">
-                <i className="fa fa-twitter"></i>
-              </a>
+    <div className="container">
+      <div className="profile-container">
+        <div className="profile-parent">
+          <div className="profile-details">
+            <div className="colz">
+              <div className="colz-icon">
+                <a href="https://web.facebook.com/md.sagor.rzs/" target="_blank">
+                  <i className="fa fa-facebook-square"></i>
+                </a>
+                <a href="https://github.com/moh-sagor" target="_blank">
+                  <i className="fa fa-github-square"></i>
+                </a>
+                <a href="https://twitter.com/s0707Sagor" target="_blank">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="profile-details-name">
-          <span className="primary-text">
-            {" "}
-            Hello, I'M <span className="highlighted-text">Sagor</span>
-          </span>
-        </div>
-        <div className="profile-details-role">
-          <span className="primary-text">
-            {" "}
-            <h1>
+          <div className="profile-details-name">
+            <span className="primary-text">
               {" "}
-              <Typical
-                loop={Infinity}
-                steps={[" Ethusiastic Developer 😎", 2000,
-                  "Full Stack Developer 💝", 2000,
-                  "React.js Developer 💕", 2000,
-                  "Django Developer 💖", 2000,
-                  "Cross Platform Developer 🌹 ", 2000,
-                ]}
-                wrapper="p"
-              />
-            </h1>
-            <span className="profile-role-tagline">
-              Knack of building applications with front and back operations.
+              Hello, I'M <span className="highlighted-text">Sagor</span>
             </span>
-          </span>
-        </div>
-        <div className="profile-options">
-          <Button className="btn">
-            {""}
-            Hire Me{""}
-          </Button>
-          <a href="Md_Sagor_Hossain.pdf" download='Md_Sagor_Hossain.pdf'>
-            <Button className='btn_1'>
-              Get Resume
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[" Ethusiastic Developer 😎", 2000,
+                    "Full Stack Developer 💝", 2000,
+                    "React.js Developer 💕", 2000,
+                    "Django Developer 💖", 2000,
+                    "Cross Platform Developer 🌹 ", 2000,
+                  ]}
+                  wrapper="p"
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <Button className="btn">
+              {""}
+              Hire Me{""}
             </Button>
-          </a>
+            <a href="Md_Sagor_Hossain.pdf" download='Md_Sagor_Hossain.pdf'>
+              <Button className='btn_1'>
+                Get Resume
+              </Button>
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="profile-picture">
-        <div className="profile-picture-background">
+        <div className="profile-picture">
+          <div className="profile-picture-background">
 
+          </div>
         </div>
       </div>
     </div>
--- src/PortfolioContainer/Resume/Resume.js
+++ src/PortfolioContainer/Resume/Resume.js
@@ -1,10 +1,158 @@
-import React from 'react'
+import * as React from "react";
+import {
+    Button,
+    Card,
+    CardBody,
+    Collapse,
+    Row,
+    Col,
+    CardTitle,
+    CardText,
+} from "reactstrap";
+// import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Resume.css";
 
 export default function Resume() {
+    const [state, setState] = React.useState({
+        articleOpen: "",
+        moreInfoOpen: true,
+    });
+    const { article, moreInfoOpen } = state;
+
+    const handleArticleOpen = (article) => {
+        setState((prevState) => ({
+            ...prevState,
+            article,
+        }));
+    };
+
     return (
-        <div className="container">
+        <div
+            className="container"
+            style={{ paddingLeft: ".5rem", justifyContent: "center" }}
+        >
+            <h1 style={{ textAlign: "center", fontFamily: "PoppinsSemiBold" }}>
+                Education
+            </h1>
+            <Collapse isOpen={moreInfoOpen}>
+                <Card className="card">
+                    <CardBody className="card-body">
+                        <div className="section section-articles">
+                            <div className="articles-buttons">
+                                <Row>
+                                    <Col sm="4">
+                                        <Button
+                                            className="article2-button"
+                                            color="primary"
+                                            onClick={() => handleArticleOpen("1")}
+                                        >
+                                            SSC
+                                        </Button>
+                                    </Col>
+                                    <Col sm="4">
+                                        <Button
+                                            className="article1-button"
+                                            color="secondary"
+                                            onClick={() => handleArticleOpen("2")}
+                                        >
+                                            HSC
+                                        </Button>
+                                    </Col>
+                                    <Col sm="4">
+                                        <Button
+                                            className="article2-button"
+                                            color="primary"
+                                            onClick={() => handleArticleOpen("3")}
+                                        >
+                                            B.Sc
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </div>
 
+                            <Collapse isOpen={article === "3"}>
+                                <aside className="profile-card">
+                                    <header>
+                                        <a target="_blank" href="#">
+                                            <img
+                                                src={require("../../assets/Resume/daffodil.jpg")}
+                                                className="hoverZoomLink"
+                                            />
+                                        </a>
 
+                                        <h1>Daffodil International University</h1>
+
+                                        <h2>Bachelor of Science in CSE</h2>
+                                    </header>
+
+                                    <div className="profile-bio">
+                                        <p>
+                                            <strong>Session : </strong>2018-2022
+                                        </p>
+                                        <p>
+                                            <strong>CGPA : </strong>3.71 out of 4
+                                        </p>
+                                    </div>
+                                </aside>
+                            </Collapse>
+
+                            <Collapse isOpen={article === "2"}>
+                                <aside className="profile-card">
+                                    <header>
+                                        <a target="_blank" href="#">
+                                            <img
+                                                src={require("../../assets/Resume/hsc.jpg")}
+                                                className="hoverZoomLink"
+                                            />
+                                        </a>
+
+                                        <h1>Govt. shaheed bulbul college, pabna</h1>
+
+                                        <h2>Higher Secondary School Certificate</h2>
+                                    </header>
+
+                                    <div className="profile-bio">
+                                        <p>
+                                            <strong>Session : </strong>2016
+                                        </p>
+                                        <p>
+                                            <strong>GPA : </strong>4.98 out of 5
+                                        </p>
+                                    </div>
+                                </aside>
+                            </Collapse>
+                            <Collapse isOpen={article === "1"}>
+                                <aside className="profile-card">
+                                    <header>
+                                        <a target="_blank" href="#">
+                                            <img
+                                                src={require("../../assets/Resume/ssc.png")}
+                                                className="hoverZoomLink"
+                                            />
+                                        </a>
+
+                                        <h1>Pabna cadet collegiate school</h1>
+
+                                        <h2>Secondary School Certificate</h2>
+                                    </header>
+
+                                    <div className="profile-bio">
+                                        <p>
+                                            <strong>Session : </strong>2014
+                                        </p>
+                                        <p>
+                                            <strong>GPA : </strong>5 out of 5
+                                        </p>
+                                    </div>
+                                </aside>
+                            </Collapse>
+                        </div>
+                        <br /> <br />
+                    </CardBody>
+                </Card>
+            </Collapse>
+            <br /> <br />
         </div>
-    )
+    );
 }
